68b57ebecc16c3208dc75a099cde5cd0357a02f9
Truong Giang <giangntse150747@gmail.com>
2023-09-25 01:57:04 +0700

    add crud maintenance apis

---
+++ Services/API_FFMS/Dtos/TaskDto.cs
@@ -0,0 +1,11 @@
+﻿using AppCore.Models;
+
+namespace API_FFMS.Dtos;
+
+public class TaskDto : BaseDto
+{
+    public DateTime RequestedDate { get; set; }
+    public DateTime? CompletionDate { get; set; }
+    public string? Description { get; set; }
+    public string? Note { get; set; }
+}
