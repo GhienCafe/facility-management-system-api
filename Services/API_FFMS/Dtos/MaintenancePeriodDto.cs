7bede2fb39b80418b0c02f9d7c96b855e2f762d7
trieudo1101 <trieudo1103@gmail.com>
2023-09-25 20:43:15 +0700

    add repairation  apis ver1

---
+++ Services/API_FFMS/Dtos/MaintenancePeriodDto.cs
@@ -0,0 +1,25 @@
+﻿using AppCore.Models;
+using MainData.Entities;
+
+namespace API_FFMS.Dtos
+{
+    public class MaintenancePeriodCreateDto
+    {
+        public Guid AssetTypeId { get; set; }
+        public TimeUnit TimeUnit { get; set; }
+        public int Period { get; set; }
+        public DateTime SpecificDate { get; set; }
+        public Guid? AssignedTo { get; set; }
+    }
+
+    public class MaintenanceScheduleConfigDetailDto : BaseDto
+    {
+        public Guid AssetTypeId { get; set; }
+        public TimeUnit TimeUnit { get; set; }
+        public int Period { get; set; }
+        public DateTime SpecificDate { get; set; }
+        public Guid? AssignedTo { get; set; }
+        public UserDto? PersonInCharge { get; set; }
+        public AssetDto? Asset { get; set; }
+    }
+}
