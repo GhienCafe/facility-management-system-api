165ef7075108e3a40687c905a4e7a7728b857fbb
Quang Nguyen <quang.nguyen20401@gmail.com>
2023-09-26 22:32:38 +0700

    have extension not have service

---
+++ Services/API_FFMS/Services/ExportService.cs
@@ -0,0 +1,15 @@
+﻿using MainData;
+using MainData.Repositories;
+
+namespace API_FFMS.Services;
+
+public interface IExportService : IBaseService
+{
+    
+}
+public class ExportService : BaseService, IExportService
+{
+    public ExportService(MainUnitOfWork mainUnitOfWork, IHttpContextAccessor httpContextAccessor, IMapperRepository mapperRepository) : base(mainUnitOfWork, httpContextAccessor, mapperRepository)
+    {
+    }
+}
