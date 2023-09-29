4822271a57c58f78160cf3a1e3fb2357acb1d875
Truong Giang <giangntse150747@gmail.com>
2023-09-30 00:09:54 +0700

    get list tasks

--- Services/API_FFMS/Dtos/TaskDto.cs
+++ Services/API_FFMS/Dtos/TaskDto.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using AppCore.Models;
+using MainData.Entities;
 
 namespace API_FFMS.Dtos;
 
@@ -15,13 +16,15 @@ public enum ActionType
     Transportation = 4,
 }
 
-public class TaskDto : BaseDto
+public class TaskDataSetDto : BaseDto
 {
     public DateTime RequestedDate { get; set; }
     public DateTime? CompletionDate { get; set; }
     public string? Description { get; set; }
     public string? Note { get; set; }
-    public ActionType ActionType { get; set; }
+    public string? Reason { get; set; }
+    public ActionStatus? Status { get; set; }
+    public ActionType? Type { get; set; }
     public Guid? AssignedTo { get; set; }
     public Guid? AssetId { get; set; }
     public UserDto? PersonInCharge { get; set; }
@@ -35,7 +38,57 @@ public class TaskDto : BaseDto
     public int? Quantity { get; set; }
     public Guid? ToRoomId { get; set; } 
     public RoomDto? ToRoom { get; set; }
+}
+
+public class TaskDto : BaseDto
+{
+    public DateTime RequestedDate { get; set; }
+    public DateTime? CompletionDate { get; set; }
+    public string? Description { get; set; }
+    public string? Note { get; set; }
+    public string? Reason { get; set; }
+    public EnumValue? Status { get; set; }
+    public EnumValue? Type { get; set; }
+    public Guid? AssignedTo { get; set; }
+    public Guid? AssetId { get; set; }
+    //public UserDto? PersonInCharge { get; set; }
+    //public AssetDto? Asset { get; set; } 
     
-    // Maintenance 
-    public EnumValue? MaintenanceType { get; set; }
+    // Replacement
+   // public Guid? NewAssetId { get; set; }
+    //public AssetDto? NewAsset { get; set; }
+    
+    // Transport
+    //public int? Quantity { get; set; }
+    public Guid? ToRoomId { get; set; } 
+    public RoomDto? ToRoom { get; set; }
+    public RoomDto? Location { get; set; }
+}
+
+public class TaskQueryDto : BaseQueryDto
+{
+}
+
+public class TaskCommonQueryDto : BaseDto
+{
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public ActionType? Type { get; set; }
+    public ActionStatus? Status { get; set; }
+    public Guid? AssetId { get; set; }
+    public Guid? RoomId { get; set; }
+    public RoomDto? Room { get; set; }
+    public DateTime? RequestedDate { get; set; }
+}
+
+public class TaskCommonDto : BaseDto
+{
+    public string? Title { get; set; }
+    public string? Content { get; set; }
+    public EnumValue? Type { get; set; }
+    public EnumValue? Status { get; set; }
+    public Guid? AssetId { get; set; }
+    public Guid? RoomId { get; set; }
+    public RoomDto? Room { get; set; }
+    public DateTime? RequestedDate { get; set; }
 }
