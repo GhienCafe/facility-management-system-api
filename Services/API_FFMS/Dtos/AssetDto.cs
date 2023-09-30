5a6b345bbb77d72093234f508544861832350cc8
Truong Giang <giangntse150747@gmail.com>
2023-09-30 14:41:24 +0700

    fix get detail task

--- Services/API_FFMS/Dtos/AssetDto.cs
+++ Services/API_FFMS/Dtos/AssetDto.cs
@@ -22,6 +22,24 @@ namespace API_FFMS.Dtos
         public AssetTypeDto? Type { get; set; }
         public ModelDto? Model { get; set; }
     }
+    
+    public class AssetBaseDto : BaseDto
+    {
+        public string AssetName { get; set; } = null!;
+        public string? AssetCode { get; set; }
+        public bool IsMovable { get; set; }
+        public AssetStatus? Status { get; set; }
+        public DateTime? ManufacturingYear { get; set; }
+        public string? SerialNumber { get; set; }
+        public double Quantity { get; set; }
+        public string? Description { get; set; }
+        public DateTime? LastMaintenanceTime { get; set; }
+        public Guid? TypeId { get; set; }
+        public Guid? ModelId { get; set; }
+        public bool? IsRented { get; set; }
+        public AssetTypeDto? Type { get; set; }
+        public ModelDto? Model { get; set; }
+    }
 
     public class AssetDetailDto : BaseDto
     {
