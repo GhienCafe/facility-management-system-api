2335adbe1693eaf83988385a8ce7244b42badb7a
Truong Giang <giangntse150747@gmail.com>
2023-10-05 15:25:05 +0700

    add base requset dto

--- Services/API_FFMS/Dtos/MaintenanceDto.cs
+++ Services/API_FFMS/Dtos/MaintenanceDto.cs
@@ -1,4 +1,10 @@
 ﻿using AppCore.Models;
 using MainData.Entities;
+using Microsoft.AspNetCore.Identity;
 
 namespace API_FFMS.Dtos;
+
+public class MaintenanceDto : BaseRequestDto {
+    public AssetBaseDto? Asset { get; set; }
+    public UserDto? User { get; set; }
+}
