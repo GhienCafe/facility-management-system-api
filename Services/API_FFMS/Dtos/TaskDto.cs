fef065f0079b1696abf24ce7eff6e67012a8e13d
trieudo1101 <trieudo1103@gmail.com>
2023-10-12 15:40:48 +0700

    task detail adpis ver1

--- Services/API_FFMS/Dtos/TaskDto.cs
+++ Services/API_FFMS/Dtos/TaskDto.cs
@@ -17,6 +17,25 @@ public class TaskBaseDto : BaseRequestDto
     //public string? NewAssetId { get; set; }
 }
 
+public class TaskDetailDto : BaseRequestDto
+{
+    //Maintenance, repair
+
+    //Replace
+    public Guid NewAssetId { get; set; }
+    public AssetBaseDto? Asset { get; set; } //New asset
+    public AssetBaseDto? NewAsset { get; set; }
+    //Transport
+    public Guid? ToRoomId { get; set; }
+    public int? Quantity { get; set; }
+    public RoomBaseDto? ToRoom { get; set; }// ToRoom
+    public List<FromRoomAssetDto>? Assets { get; set; }
+
+    //Asset check
+    //public AssetBaseDto? Asset { get; set; }
+    //public RoomBaseDto? Location { get; set; }
+}
+
 public class TaskQueryDto : BaseRequestQueryDto
 {
     public RequestType Type { get; set; }
