c11ef69c030ea96557bb1017cca36cf1677ac5a8
Quang Nguyen <quang.nguyen20401@gmail.com>
2023-10-12 16:36:20 +0700

    noti common

--- Workers/Worker_Notify/Worker.cs
+++ Workers/Worker_Notify/Worker.cs
@@ -1,29 +1,37 @@
+using System;
+using System.Threading;
 using Worker_Notify.Services;
 
-namespace Worker_Notify;
-
-public class Worker : BackgroundService
+namespace Worker_Notify
 {
-    private readonly ILogger<Worker> _logger;
-    private readonly IServiceProvider _serviceProvider;
-
-    public Worker(ILogger<Worker> logger, IServiceProvider serviceProvider)
+    public class Worker : BackgroundService
     {
-        _logger = logger;
-        _serviceProvider = serviceProvider;
-    }
+        private readonly ILogger<Worker> _logger;
+        private readonly IServiceProvider _serviceProvider;
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        while (!stoppingToken.IsCancellationRequested)
+        public Worker(ILogger<Worker> logger, IServiceProvider serviceProvider)
         {
-            using (var scope = _serviceProvider.CreateScope())
+            _logger = logger;
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
             {
-                var pushNotificationService = scope.ServiceProvider.GetRequiredService<IPushNotificationService>();
-                await pushNotificationService.SendAndMarkAsSent();
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var appNotificationService = scope.ServiceProvider.GetRequiredService<IShortTermNotificationService>();
+                    await Task.Run(() => appNotificationService.SendTask());
+                    
+                    var delay = TimeSpan.FromSeconds(1);
+                    // var delay = TimeSpan.FromDays(30);
+                    var webNotificationService = scope.ServiceProvider.GetRequiredService<IWebNotificationService>();
+                    await Task.Run(() =>  webNotificationService.SendMaintenance(delay));
+                }
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
             }
-            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
         }
     }
 }
