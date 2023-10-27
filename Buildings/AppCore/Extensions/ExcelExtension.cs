87ad2723df0c30150f7952642ca1d4f42bf7c2ff
trieudo1101 <trieudo1103@gmail.com>
2023-10-27 20:53:09 +0700

    add export multi task

--- Buildings/AppCore/Extensions/ExcelExtension.cs
+++ Buildings/AppCore/Extensions/ExcelExtension.cs
@@ -131,6 +131,8 @@ public static class ExportHelperList<T>
         return stream;
     }
 
+
+
     public static Stream ExportV2(List<T> items, string sheetName, string title, int startRow = 6,
         Dictionary<string, string> otherFields = null)
     {
@@ -221,6 +223,142 @@ public static class ExportHelperList<T>
         worksheet.Columns();
         workbook.SaveAs(stream);
         workbook.Dispose();
+        stream.Position = 0;
+        return stream;
+    }
+
+    public static Stream ExportUltra(List<List<T>> itemsList, List<string> sheetNames, List<string> titles, int startRow = 6, Dictionary<string, string> otherFields = null)
+    {
+        if (itemsList == null || itemsList.Count == 0 || sheetNames == null || sheetNames.Count == 0 || titles == null || titles.Count == 0)
+        {
+            throw new ArgumentException("Invalid input data.");
+        }
+
+        var now = DateTime.Now;
+
+        var stream = new MemoryStream();
+
+        using (var workbook = new XLWorkbook())
+        {
+            for (int sheetIndex = 0; sheetIndex < sheetNames.Count; sheetIndex++)
+            {
+                var sheetName = sheetNames[sheetIndex];
+                var title = titles[sheetIndex];
+                var items = itemsList[sheetIndex]; // Get the items for the current sheet
+
+                var defaultColumns = new Dictionary<string, ExportField>();
+                var valueType = typeof(T);
+                var propertyInfos = valueType.GetProperties();
+
+                // Filter out properties with all null values
+                var availableProperties = propertyInfos.Where(propInfo => items.Any(item => item.GetType().GetProperty(propInfo.Name)?.GetValue(item) != null)).ToList();
+
+                foreach (var propertyInfo in availableProperties)
+                {
+                    var name = propertyInfo.GetCustomAttribute<DisplayAttribute>()?.Name;
+                    if (!string.IsNullOrEmpty(name))
+                    {
+                        defaultColumns.Add(propertyInfo.Name, new ExportField
+                        {
+                            Name = name,
+                            Type = propertyInfo.PropertyType.Name
+                        });
+                    }
+                }
+
+                var columnCount = defaultColumns.Count + 1;
+
+                var worksheet = workbook.Worksheets.Add(sheetName);
+
+                // Title
+                worksheet.Cell(1, 1).SetValue(title.ToUpper());
+                worksheet.Cell(1, 1).Style.Font.FontSize = 18;
+                worksheet.Cell(1, 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                worksheet.Cell(1, 1).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+                worksheet.Range(1, 1, 1, columnCount).Row(1).Merge();
+                worksheet.Range(1, 1, 1, columnCount).Style.Font.Bold = true;
+
+                // Time export
+                worksheet.Cell(2, 1).SetValue($"Thời gian xuất file: {now:yy-MM-dd hh:mm:ss}");
+                worksheet.Cell(2, 1).Style.Font.FontSize = 12;
+                worksheet.Cell(2, 1).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                worksheet.Cell(2, 1).Style.Alignment.Vertical = XLAlignmentVerticalValues.Center;
+                worksheet.Range(2, 1, 2, columnCount).Row(1).Merge();
+
+                // Header
+                worksheet.Cell(5, 1).Value = "STT";
+                var currentColumn = 2;
+                var columnsConvert = defaultColumns.Count(x => x.Value.Index == 0) > 1
+                    ? defaultColumns.ToList()
+                    : defaultColumns.OrderBy(x => x.Value.Index).ToList();
+                foreach (var column in columnsConvert)
+                {
+                    var propertyName = column.Key;
+
+                    // Check if all values in this column are null, if so, skip it
+                    if (!items.Any(item => item.GetType().GetProperty(propertyName)?.GetValue(item) != null))
+                    {
+                        continue;
+                    }
+
+                    worksheet.Cell(5, currentColumn).SetValue(column.Value.Name);
+                    currentColumn++;
+                }
+
+                worksheet.Range(5, 1, 5, columnCount).Style.Fill.BackgroundColor = XLColor.FromArgb(52, 168, 83);
+                worksheet.Range(5, 1, 5, columnCount).Style.Font.FontColor = XLColor.White;
+
+                // Other fields
+                foreach (var otherField in otherFields ?? new Dictionary<string, string>())
+                {
+                    worksheet.Cell(otherField.Key).Value = otherField.Value;
+                }
+
+                // Content Start col = 6
+                var currentRow = startRow >= 6 ? startRow : 6;
+                foreach (var item in CollectionsMarshal.AsSpan(items))
+                {
+                    currentColumn = 2;
+                    foreach (var column in columnsConvert)
+                    {
+                        var propertyName = column.Key;
+
+                        // Check if all values in this column are null, if so, skip it
+                        if (!items.Any(item => item.GetType().GetProperty(propertyName)?.GetValue(item) != null))
+                        {
+                            currentColumn++;
+                            continue;
+                        }
+
+                        var value = valueType.GetProperty(propertyName)?.GetValue(item);
+
+                        // Special handling for "Danh sách thiết bị" column
+                        if (propertyName == "Assets")
+                        {
+                            var assetTransportExportDtos = (List<AssetTransportExportDto>)value;
+                            var assetNames = assetTransportExportDtos != null
+                                ? string.Join(Environment.NewLine, assetTransportExportDtos.Select((a, index) => $"{index + 1}. {a.AssetName}"))
+                                : string.Empty;
+                            worksheet.Cell(currentRow, currentColumn).Value = assetNames;
+                            //worksheet.Cell(currentRow, currentColumn).Style.Alignment.SetWrapText();
+                            if ((value?.ToString() ?? string.Empty).Length > 100)
+                                worksheet.Cell(currentRow, currentColumn).Style.Alignment.WrapText = true;
+                        }
+                        else
+                        {
+                            // Set other column values as before
+                            worksheet.Cell(currentRow, currentColumn).Value = value?.ToString();
+
+                            if ((value?.ToString() ?? string.Empty).Length > 50)
+                                worksheet.Cell(currentRow, currentColumn).Style.Alignment.WrapText = true;
+                        }
+                        currentColumn++;
+                    }
+                    currentRow++;
+                }
+            }
+            workbook.SaveAs(stream);
+        }
         stream.Position = 0;
         return stream;
     }
