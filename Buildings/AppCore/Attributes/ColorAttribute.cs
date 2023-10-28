2e75758204c7074d869481d842558ae0dfaa93c6
Truong Giang <giangntse150747@gmail.com>
2023-10-28 12:23:18 +0700

    return color for status enum

---
+++ Buildings/AppCore/Attributes/ColorAttribute.cs
@@ -0,0 +1,13 @@
+﻿namespace AppCore.Attributes;
+
+[AttributeUsage(AttributeTargets.Field, Inherited = false, AllowMultiple = false)]
+public sealed class ColorAttribute : Attribute
+{
+    public string Color { get; }
+
+    public ColorAttribute(string color)
+    {
+        Color = color;
+    }
+}
+
