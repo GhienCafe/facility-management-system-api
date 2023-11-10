4577b31ee399d2af374a1700d4cb85e9731001ef
trieudo1101 <trieudo1103@gmail.com>
2023-11-10 12:27:33 +0700

    update creat inventory-check

--- Services/API_FFMS/Repositories/InventoryCheckRepository.cs
+++ Services/API_FFMS/Repositories/InventoryCheckRepository.cs
@@ -7,6 +7,7 @@ namespace API_FFMS.Repositories;
 public interface IInventoryCheckRepository
 {
     Task<bool> InsertInventoryCheck(InventoryCheck inventoryCheck, List<Asset?> assets, Guid? creatorId, DateTime? now = null);
+    Task<bool> InsertInventoryCheckV2(InventoryCheck inventoryCheck, List<Room?> rooms, Guid? creatorId, DateTime? now = null);
 
     //Task<bool> UpdateMaintenanceScheduleConfig(MaintenanceScheduleConfig maintenanceScheduleConfig, IEnumerable<Asset>? oldData, IEnumerable<Asset>? newData, Guid? editorId, DateTime? now = null);
 }
@@ -58,10 +59,73 @@ public class InventoryCheckRepository : IInventoryCheckRepository
                     InventoryCheckId = inventoryCheck.Id,
                     CreatorId = creatorId,
                     CreatedAt = now.Value,
-                    RoomId = inventoryCheck.RoomId,
                     Status = asset.Status
                 };
                 await _context.InventoryCheckDetails.AddAsync(inventoryCheckDetail);
+            }
+
+            var notification = new Notification
+            {
+                CreatedAt = now.Value,
+                EditedAt = now.Value,
+                Status = NotificationStatus.Waiting,
+                Content = inventoryCheck.Description,
+                Title = RequestType.Maintenance.GetDisplayName(),
+                Type = NotificationType.Task,
+                CreatorId = creatorId,
+                IsRead = false,
+                ItemId = inventoryCheck.Id,
+                UserId = inventoryCheck.AssignedTo
+            };
+            await _context.Notifications.AddAsync(notification);
+
+            await _context.SaveChangesAsync();
+            await _context.Database.CommitTransactionAsync();
+            return true;
+        }
+        catch
+        {
+            await _context.Database.RollbackTransactionAsync();
+            return false;
+        }
+    }
+
+    public async Task<bool> InsertInventoryCheckV2(InventoryCheck inventoryCheck, List<Room?> rooms, Guid? creatorId, DateTime? now = null)
+    {
+        await _context.Database.BeginTransactionAsync();
+        now ??= DateTime.UtcNow;
+        try
+        {
+            inventoryCheck.Id = Guid.NewGuid();
+            inventoryCheck.CreatedAt = now.Value;
+            inventoryCheck.EditedAt = now.Value;
+            inventoryCheck.CreatorId = creatorId;
+            inventoryCheck.Status = RequestStatus.NotStart;
+            inventoryCheck.RequestDate = now.Value;
+            await _context.InventoryChecks.AddAsync(inventoryCheck);
+
+            foreach (var room in rooms)
+            {
+                var roomAssets = _context.RoomAssets
+                                         .Where(ra => ra.RoomId == room!.Id && ra.ToDate == null)
+                                         .Select(ra => ra.Asset)
+                                         .ToList();
+
+                foreach (var asset in roomAssets)
+                {
+                    var inventoryCheckDetail = new InventoryCheckDetail
+                    {
+                        Id = Guid.NewGuid(),
+                        AssetId = asset!.Id,
+                        InventoryCheckId = inventoryCheck.Id,
+                        CreatorId = creatorId,
+                        CreatedAt = now.Value,
+                        RoomId = room!.Id,
+                        Status = asset.Status
+                    };
+
+                    await _context.InventoryCheckDetails.AddAsync(inventoryCheckDetail);
+                }
             }
 
             var notification = new Notification
