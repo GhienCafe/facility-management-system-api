69a3097a49383b86fe0e4b0b311e52e4ae361c62
Truong Giang <giangntse150747@gmail.com>
2023-11-27 20:17:39 +0700

    increase performance

--- Services/API_FFMS/Services/DashboardService.cs
+++ Services/API_FFMS/Services/DashboardService.cs
@@ -11,7 +11,6 @@ namespace API_FFMS.Services;
 
 public interface IDashboardService : IBaseService
 {
-    public Task<ApiResponse<DashboardDto>> GetInformation();
     public Task<ApiResponse<IEnumerable<AssetDashBoardInformation>>> GetAssetStatusInformation();
     public Task<ApiResponse<IEnumerable<TaskDashBoardInformation>>> GetBaseTaskInformation();
     public Task<ApiResponse<IEnumerable<TaskBasedOnStatusDashboardDto>>> GetBaseTaskStatusInformation();
@@ -27,51 +26,7 @@ public class DashboardService : BaseService, IDashboardService
     {
 
     }
-
-    public async Task<ApiResponse<DashboardDto>> GetInformation()
-    {
-        var assetDataSet = MainUnitOfWork.AssetRepository.GetQuery().Where(x => !x!.DeletedAt.HasValue);
-        var typeAssetDataSet = MainUnitOfWork.AssetTypeRepository.GetQuery().Where(x => !x!.DeletedAt.HasValue);
-        var assetTypeData = typeAssetDataSet.Select(assetType => new AssetTypeDashboardDto
-        {
-            TypeId = assetType!.Id,
-            TypeName = assetType.TypeName,
-            Quantity = assetDataSet
-                .Where(asset => asset!.TypeId == assetType.Id)
-                .Sum(asset => asset!.Quantity),
-            InUsed = assetDataSet
-                .Where(asset => asset!.TypeId == assetType.Id && asset.Status == AssetStatus.Operational)
-                .Sum(asset => asset!.Quantity),
-            NotUsed = assetDataSet
-                .Where(asset => asset!.TypeId == assetType.Id && asset.Status == AssetStatus.Inactive)
-                .Sum(asset => asset!.Quantity),
-            Maintenance = assetDataSet
-                .Count(asset => asset!.TypeId == assetType.Id && asset.Status == AssetStatus.Maintenance),
-            Repair = assetDataSet
-                .Count(asset => asset!.TypeId == assetType.Id && asset.Status == AssetStatus.Repair),
-            Transportation = assetDataSet
-                .Count(asset => asset!.TypeId == assetType.Id && asset.Status == AssetStatus.Transportation),
-            Replacement = assetDataSet
-                .Count(asset => asset!.TypeId == assetType.Id && asset.Status == AssetStatus.Replacement),
-            NeedInspection = assetDataSet
-                .Count(asset => asset!.TypeId == assetType.Id && asset.Status == AssetStatus.NeedInspection),
-        }).ToList();
-        var dashboardDto = new DashboardDto
-        {
-            TotalQuantity = assetTypeData.Sum(asset => asset.Quantity),
-            TotalInUsed = assetTypeData.Sum(asset => asset.InUsed),
-            TotalNotUsed = assetTypeData.Sum(asset => asset.NotUsed),
-            TotalMaintenance = assetTypeData.Sum(asset => asset.Maintenance),
-            TotalRepair = assetTypeData.Sum(asset => asset.Repair),
-            TotalTransportation = assetTypeData.Sum(asset => asset.Transportation),
-            TotalReplacement = assetTypeData.Sum(asset => asset.Replacement),
-            TotalNeedInspection = assetTypeData.Sum(asset => asset.NeedInspection),
-            AssetType = assetTypeData,
-        };
-        await Task.CompletedTask;
-        return ApiResponse<DashboardDto>.Success(dashboardDto);
-    }
-
+    
     public async Task<ApiResponse<IEnumerable<AssetDashBoardInformation>>> GetAssetStatusInformation()
     {
         var assetDashboardInfo = await MainUnitOfWork.AssetRepository.GetQuery()
