a2831d6b38111fbf600c43b8f7b34496c8f8e3cc
Truong Giang <giangntse150747@gmail.com>
2023-11-27 19:34:27 +0700

    add service background for inventory config

--- Workers/Worker_Notify/Workers/InventoryWorker.cs
+++ Workers/Worker_Notify/Workers/InventoryWorker.cs
@@ -1,24 +1,63 @@
-﻿using MainData;
-using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Worker_Notify.Services;
 
-namespace Worker_Notify.Workers;
-
-public class InventoryWorker : BackgroundService
+namespace Worker_Notify.Workers
 {
-    private readonly IServiceScopeFactory _serviceScopeFactory;
-
-    public InventoryWorker(IServiceScopeFactory serviceScopeFactory)
+    public class InventoryWorker : BackgroundService
     {
-        _serviceScopeFactory = serviceScopeFactory ?? throw new ArgumentNullException(nameof(serviceScopeFactory));
-    }
+        private readonly ILogger<InventoryWorker> _logger;
+        private readonly IInventoryConfigService _inventoryConfigService;
+        private readonly TimeZoneInfo _localTimeZone = TimeZoneInfo.Local;
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        while (!stoppingToken.IsCancellationRequested)
+        private Timer _timer;
+
+        public InventoryWorker(ILogger<InventoryWorker> logger, IInventoryConfigService inventoryConfigService)
         {
-         
+            _logger = logger;
+            _inventoryConfigService = inventoryConfigService;
+        }
 
-            await Task.Delay(TimeSpan.FromHours(24), stoppingToken); 
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            _timer = new Timer(DoWork, null, GetDelay(), TimeSpan.FromHours(24)); // Khởi động timer với thời gian delay
+
+            // Đợi cho sự kết thúc hoặc hủy bỏ của công việc
+            await Task.Delay(Timeout.Infinite, stoppingToken);
+        }
+
+        private TimeSpan GetDelay()
+        {
+            DateTime now = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, _localTimeZone);
+            DateTime scheduledTime = new DateTime(now.Year, now.Month, now.Day, 6, 0, 0); // Lúc 19h20
+            if (scheduledTime <= now)
+            {
+                scheduledTime = scheduledTime.AddDays(1); // Nếu đã qua thời gian đã lên lịch, chuyển sang ngày tiếp theo
+            }
+            TimeSpan delay = scheduledTime - now;
+            return delay < TimeSpan.Zero ? TimeSpan.Zero : delay;
+        }
+
+        private void DoWork(object state)
+        {
+            DateTime localTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, _localTimeZone);
+            _inventoryConfigService.CreateSystemNotification();
+            _logger.LogInformation("Worker running at: {time}", localTime);
+            _timer.Change(GetDelay(), TimeSpan.FromHours(24)); // Đặt lại timer cho ngày tiếp theo
+        }
+
+        public override async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+            await base.StopAsync(cancellationToken);
+        }
+
+        public override void Dispose()
+        {
+            _timer?.Dispose();
+            base.Dispose();
         }
     }
 }
